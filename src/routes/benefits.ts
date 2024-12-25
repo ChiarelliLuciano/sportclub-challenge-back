e9d6f9f0c6133fe5630e872f476ecf37dcf60987
Luciano <luciano.miguel.ch95@gmail.com>
2024-12-25 12:53:46 -0300

    add cache

--- src/routes/benefits.ts
+++ src/routes/benefits.ts
@@ -1,45 +1,71 @@
 import axios from 'axios';
 import express from 'express';
 import { API_BASE_URL } from '../config/config';
+import NodeCache from 'node-cache';
 
 const router = express.Router();
+const memoryCache = new NodeCache();
 
-router.get('/api/beneficios', async (req, res, next) => {
-    try {
-        const response = await axios.get(API_BASE_URL);
+function getFromCache(key: string): any {
+    return memoryCache.get(key);
+}
+function setInCache(key: string, value: any, ttl: number): void {
+    memoryCache.set(key, value, ttl); // Time to Live in seconds
+}
 
-        if (response.data) {
-            logging.info('Fetched benefits successfully', response.data);
+router.get('/api/beneficios', (req, res, next) => {
+    (async () => {
+        const cacheKey = 'allBenefits';
 
-            res.status(200).json(response.data);
-        } else {
-            const error = new Error('No benefits were found');
-            throw error;
+        try {
+            const cachedData = getFromCache(cacheKey);
+            if (cachedData) {
+                logging.info('Serving data from cache', cachedData);
+                return res.status(200).json(cachedData);
+            }
+
+            const response = await axios.get(API_BASE_URL);
+            if (response.data) {
+                logging.info('Fetched benefits successfully', response.data);
+
+                setInCache(cacheKey, response.data, 180);
+
+                return res.status(200).json(response.data);
+            } else {
+                throw new Error('No benefits were found');
+            }
+        } catch (error: any) {
+            next(error); // Pass error to errorHandler
         }
-    } catch (error: any) {
-        logging.error(error.message);
-
-        next(error); // Pass error to errorHandler
-    }
+    })();
 });
 
-router.get('/api/beneficios/:id', async (req, res, next) => {
-    const { id } = req.params;
-    try {
-        const response = await axios.get(`${API_BASE_URL}/${id}`);
-        if (response.data) {
-            logging.info(`Fetched benefit with id ${id} successfully`, response.data);
+router.get('/api/beneficios/:id', (req, res, next) => {
+    (async () => {
+        const { id } = req.params;
+        const cacheKey = `singleBenefitId:${id}`;
 
-            res.status(200).json(response.data);
-        } else {
-            const error = new Error(`No benefit with ID: ${id}`);
-            throw error;
+        try {
+            const cachedData = getFromCache(cacheKey);
+            if (cachedData) {
+                logging.info('Serving data from cache', cachedData);
+                return res.status(200).json(cachedData);
+            }
+
+            const response = await axios.get(`${API_BASE_URL}/${id}`);
+            if (response.data) {
+                logging.info(`Fetched benefit with ID ${id} successfully`, response.data);
+
+                setInCache(cacheKey, response.data, 180);
+
+                return res.status(200).json(response.data);
+            } else {
+                throw new Error(`No benefit with ID: ${id}`);
+            }
+        } catch (error: any) {
+            next(error); // Pass error to errorHandler
         }
-    } catch (error: any) {
-        logging.error(error.message);
-
-        next(error); // Pass error to errorHandler
-    }
+    })();
 });
 
 export default router;
